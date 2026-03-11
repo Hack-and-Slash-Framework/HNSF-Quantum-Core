4f4de231de9f8072eab417120f0e6c59226255a0
Chris Collins <29593332+christides11@users.noreply.github.com>
2026-03-10 23:17:49 -0700

    fix: Ignore movement input when movement disabled

--- Simulation/Core/Input/ActorInputBufferMovement.cs
+++ Simulation/Core/Input/ActorInputBufferMovement.cs
@@ -6,12 +6,14 @@ namespace Quantum
     {
         public FPVector2 GetMovement(int offset = 0)
         {
+            if (disableReadMovement) return FPVector2.Zero;
             if (offset >= Constants.MOVEMENT_BUFFER_SIZE) offset = Constants.MOVEMENT_BUFFER_SIZE - 1;
             return buffer[(bufferPosition - offset) % (Constants.MOVEMENT_BUFFER_SIZE)];
         }
 
         public FPVector2 GetFirstMovementInput(short offset, FP validMagnitude, bool returnZeroIfNone = true)
         {
+            if (disableReadMovement) return FPVector2.Zero;
             if (offset >= Constants.CAMERA_BUFFER_SIZE) offset = Constants.CAMERA_BUFFER_SIZE - 1;
             for (int i = offset; i < Constants.CAMERA_BUFFER_SIZE; i++)
             {
