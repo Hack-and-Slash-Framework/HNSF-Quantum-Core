1a5e220c794cbc1e500cea22d8545641cc5e7389
Chris Collins <29593332+christides11@users.noreply.github.com>
2026-05-24 14:49:27 -0700

    feat: Improved sound management

--- Scripts/Sound/EntitySoundManager.cs
+++ Scripts/Sound/EntitySoundManager.cs
@@ -10,11 +10,32 @@ namespace HnSF
     {
         private Dictionary<GameAudioSource, AssetRef<Tag>> audioSourceToTag = new();
         private Dictionary<GameAudioSource, SoundEntry> audioSourceToSoundEntry = new();
-
+        public Dictionary<GameAudioSource, Vector3> parentedAudioSources = new();
+        
         private Dictionary<SoundEntry, List<GameAudioSource>> currentlyPlayingSoundTypes = new();
         public Dictionary<AssetRef<Tag>, List<GameAudioSource>> currentlyPlayingSounds = new();
 
         [NonSerialized] public Dictionary<AudioSourceConfig, ObjectPool<GameAudioSource>> audioPool = null;
+        [NonSerialized] public EntitySoundManager parentManager;
+
+        private void Awake()
+        {
+            if (EntityView == null)
+                return;
+            
+            EntityView.OnEntityInstantiated.AddListener(WhenEntityInstantiated);
+            EntityView.OnEntityDestroyed.AddListener(WhenEntityDestroyed);
+        }
+        
+        protected virtual void WhenEntityInstantiated(QuantumGame arg0)
+        {
+            
+        }
+
+        protected virtual void WhenEntityDestroyed(QuantumGame arg0)
+        {
+            parentManager?.InheritSoundsFrom(this);
+        }
 
         public override void OnUpdateView()
         {
@@ -25,16 +46,52 @@ namespace HnSF
                     if (cpList.Value[i].audioSource.isPlaying == false) StopSound(cpList.Value[i]);
                 }
             }
+
+            foreach (var parentedSource in parentedAudioSources)
+            {
+                
+            }
         }
 
+        public virtual void InheritSoundsFrom(EntitySoundManager otherSoundManager)
+        {
+            foreach (var soundList in otherSoundManager.currentlyPlayingSounds)
+            {
+                var list = soundList.Value;
+
+                foreach (var aSource in soundList.Value)
+                {
+                    if(aSource == null)
+                        continue;
+                    
+                    RegisterSound(aSource);
+                }
+            }
+        }
+
+        public virtual void RegisterSound(GameAudioSource audioSource)
+        {
+            var qTag = audioSource.soundEntry.tag;
+            if (!currentlyPlayingSounds.ContainsKey(qTag))
+                currentlyPlayingSounds.Add(qTag, new List<GameAudioSource>());
+
+            if (currentlyPlayingSounds[qTag].Contains(audioSource)) return;
+            
+            currentlyPlayingSounds[qTag].Add(audioSource);
+            
+            audioSourceToTag.Add(audioSource, qTag);
+            audioSourceToSoundEntry.Add(audioSource, audioSource.soundEntry);
+            currentlyPlayingSoundTypes[audioSource.soundEntry].Add(audioSource);
+        }
+        
         public virtual bool PlaySound(GameAudioSource audioSource, SoundEntry soundEntry, GameObject parent, Vector3 position,
             float time, float volume, float pitch, AssetRef<Tag> qTag,
             AudioSourceConfig audioSourceConfig, EventKey key, bool stopOtherInstances = false,
             bool stopOthersOfSameTag = false,
             bool ignoreIfSoundPlaying = false, bool ignoreIfTagPlaying = false)
         {
-            if (parent) audioSource.transform.SetParent(parent.transform);
-
+            if(parent) parentedAudioSources.Add(audioSource, audioSource.gameObject.transform.position - transform.position);
+            
             //if (stopOthersOfSameTag && currentlyPlayingSounds.ContainsKey(tag)) 
             if (!currentlyPlayingSounds.ContainsKey(qTag))
                 currentlyPlayingSounds.Add(qTag, new List<GameAudioSource>());
@@ -86,11 +143,13 @@ namespace HnSF
             currentlyPlayingSoundTypes[soundEntry].Clear();
         }
 
-        public virtual void StopSound(GameAudioSource audioSource)
+        public virtual void StopSound(GameAudioSource audioSource, bool release = true)
         {
             if (!audioSourceToTag.TryGetValue(audioSource, out var asTag)
                 || !audioSourceToSoundEntry.TryGetValue(audioSource, out var soundEntry)) return;
-
+            
+            parentedAudioSources.Remove(audioSource);
+            
             currentlyPlayingSounds[asTag].Remove(audioSource);
             currentlyPlayingSoundTypes[soundEntry].Remove(audioSource);
 
@@ -99,6 +158,9 @@ namespace HnSF
 
             audioSource.audioSource.Stop();
 
+            if (!release)
+                return;
+            
             if (audioPool != null && audioPool.ContainsKey(audioSource.config))
             {
                 audioPool[audioSource.config].Release(audioSource);
--- Scripts/Sound/GameAudioSource.cs
+++ Scripts/Sound/GameAudioSource.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Quantum;
 using UnityEngine;
 
@@ -5,7 +6,11 @@ namespace HnSF
 {
     public class GameAudioSource : MonoBehaviour
     {
+        public SoundEntry soundEntry;
         public AudioSourceConfig config;
         public AudioSource audioSource;
+        public EntitySoundManager owner;
+
+        public List<Vector3Int> inSlices;
     }
 }
