972316099854aedc7c515f5ce5b163dd4275597b
Chris Collins <29593332+christides11@users.noreply.github.com>
2026-07-09 22:39:27 -0700

    feat: Helpers for clip import settings

---
+++ Editor/Helper/ClipImportSettingsHelper.cs
@@ -0,0 +1,92 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace HnSF
+{
+    public static class ClipImportSettingsHelper
+    {
+        public static bool TryGetClipRotationOffsetY(this AnimationClip clip, out float eulerOffsetY)
+        {
+            eulerOffsetY = 0f;
+
+            if (clip == null)
+                return false;
+
+            string path = AssetDatabase.GetAssetPath(clip);
+            if (string.IsNullOrEmpty(path))
+                return false;
+
+            if (AssetImporter.GetAtPath(path) is not ModelImporter importer)
+                return false;
+
+            var clips = importer.clipAnimations;
+            if (clips == null || clips.Length == 0)
+                clips = importer.defaultClipAnimations;
+
+            foreach (var clipSettings in clips)
+            {
+                if (clipSettings.name != clip.name)
+                    continue;
+
+                eulerOffsetY = clipSettings.rotationOffset;
+                return true;
+            }
+
+            return false;
+        }
+        
+        public static bool TryGetClipOffsetY(this AnimationClip clip, out float offsetY)
+        {
+            offsetY = 0f;
+
+            if (clip == null)
+                return false;
+
+            string path = AssetDatabase.GetAssetPath(clip);
+            if (string.IsNullOrEmpty(path))
+                return false;
+
+            if (AssetImporter.GetAtPath(path) is not ModelImporter importer)
+                return false;
+
+            var clips = importer.clipAnimations;
+            if (clips == null || clips.Length == 0)
+                clips = importer.defaultClipAnimations;
+
+            foreach (var clipSettings in clips)
+            {
+                if (clipSettings.name != clip.name)
+                    continue;
+
+                offsetY = clipSettings.heightOffset;
+                return true;
+            }
+
+            return false;
+        }
+        
+        public static bool TryGetStandaloneClipOrientationOffsetY(this AnimationClip clip, out float eulerOffsetY)
+        {
+            eulerOffsetY = 0f;
+
+            if (clip == null)
+                return false;
+
+            var settings = AnimationUtility.GetAnimationClipSettings(clip);
+            eulerOffsetY = settings.orientationOffsetY;
+            return true;
+        }
+        
+        public static bool TryGetStandaloneClipOffsetY(this AnimationClip clip, out float offsetY)
+        {
+            offsetY = 0f;
+
+            if (clip == null)
+                return false;
+
+            var settings = AnimationUtility.GetAnimationClipSettings(clip);
+            offsetY = settings.level;
+            return true;
+        }
+    }
+}
