04d6f2215005bd3be8459e4a7f54a73e51ab0014
Chris Collins <29593332+christides11@users.noreply.github.com>
2026-08-18 20:28:51 -0700

    fix: Only execute SensorGroup while Executing

--- Simulation/Core/AI/HTN/Sensors/SensorGroup.cs
+++ Simulation/Core/AI/HTN/Sensors/SensorGroup.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 
 namespace HnSF.core.AI.HTN.Sensors
 {
-    public class SensorGroup : AssetObject
+    public unsafe class SensorGroup : AssetObject
     {
 #if QUANTUM_UNITY
         [SerializeReference, SubclassSelector]
 #endif
         public List<ISensor> sensors = new List<ISensor>();
         
-        public virtual void Execute(ref HTNAgentContext context)
+        public virtual void Execute(ref HTNAgentContext context, bool executingOnly = true)
         {
+            if (executingOnly && context.agent->contextState != HTNContextState.Executing)
+                return;
+            
             foreach (var sensor in sensors)
             {
                 sensor.Execute(ref context);
